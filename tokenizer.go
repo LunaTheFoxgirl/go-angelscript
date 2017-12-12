073954528727ae9bf836008e59eea4e66c19cb06
Member1221 <TheisCHR@live.dk>
2017-12-12 15:10:07 +0100

    fixed compilation/gofmt issues

--- tokenizer.go
+++ tokenizer.go
@@ -2,9 +2,10 @@ package angelscript
 
 import (
 	_ "encoding/binary"
-	_ "reflect"
 	"github.com/Member1221/go-angelscript/flags"
+	_ "reflect"
 )
+
 //Token definition
 
 type Token = uint32
@@ -144,17 +145,17 @@ const (
 
 const (
 	ASWhitespaceToken = " \t\r\n"
-	ASThisToken = "this"
-	ASFromToken = "from"
-	ASSuperToken = "super"
-	ASSharedToken = "shared"
-	ASFinalToken = "final"
-	ASOverrideToken = "override"
-	ASGetToken = "get"
-	ASSetToken = "set"
-	ASAbstractToken = "abstract"
-	ASFunctionToken = "function"
-	ASIfHandleToken = "if_handle_then_const"
+	ASThisToken       = "this"
+	ASFromToken       = "from"
+	ASSuperToken      = "super"
+	ASSharedToken     = "shared"
+	ASFinalToken      = "final"
+	ASOverrideToken   = "override"
+	ASGetToken        = "get"
+	ASSetToken        = "set"
+	ASAbstractToken   = "abstract"
+	ASFunctionToken   = "function"
+	ASIfHandleToken   = "if_handle_then_const"
 )
 
 type TokenWord struct {
@@ -276,7 +277,7 @@ var tokenWords []TokenWord = []TokenWord{
 }
 
 type Tokenizer struct {
-	engine int
+	engine       int
 	keywordTable map[string][]TokenWord
 }
 
@@ -286,11 +287,11 @@ func NewTokenizer() *Tokenizer {
 	for i := 0; i < len(tokenWords); i++ {
 		current := tokenWords[i]
 		start := current.Word[0:1]
-		
+
 		if tk.keywordTable[start] == nil {
 			tk.keywordTable[start] = make([]TokenWord, 32)
 		}
-		
+
 		insert, index := 0, 0
 		for _, w := range tk.keywordTable[start] {
 			if w.Length >= current.Length {
@@ -299,11 +300,11 @@ func NewTokenizer() *Tokenizer {
 			index++
 		}
 		for index > insert {
-			tk.keywordTable[start][index] = tk.keywordTable[start][index - 1]
+			tk.keywordTable[start][index] = tk.keywordTable[start][index-1]
 			index--
 		}
 		tk.keywordTable[start][insert] = current
-		
+
 	}
 	return &tk
 }
@@ -314,13 +315,13 @@ func (tk *Tokenizer) Cleanup() {
 
 func (tk *Tokenizer) IsDigitinRadix(ch rune, radix int) bool {
 	if ch >= '0' && ch <= '9' {
-		return int(ch - '0') < radix;
+		return int(ch-'0') < radix
 	}
 	if ch >= 'A' && ch <= 'Z' {
-		return int(ch - 'A'-10) < radix;
+		return int(ch-'A'-10) < radix
 	}
 	if ch >= 'a' && ch <= 'z' {
-		return int(ch - 'a'-10) < radix;
+		return int(ch-'a'-10) < radix
 	}
 	return false
 }
@@ -347,19 +348,19 @@ func (tk *Tokenizer) ParseToken(source string) (flags.ASTokenClass, uint32, Toke
 	if ok, l, token := tk.IsKeyword(source); ok == true {
 		return flags.ASTokenWhitespace, l, token
 	}
-	
+
 	return flags.ASTokenUnknown, 1, ttUnrecognizedToken
 }
 
 func (tk *Tokenizer) IsWhiteSpace(source string) (bool, uint32, Token) {
-	
-	if 	len(source) >= 3 && 
-		uint32(source[0]) == 0xEF && 
-		uint32(source[1]) == 0xBB && 
+
+	if len(source) >= 3 &&
+		uint32(source[0]) == 0xEF &&
+		uint32(source[1]) == 0xBB &&
 		uint32(source[2]) == 0xBF {
 		return true, 3, ttWhiteSpace
 	}
-	
+
 	n := 0
 	numWsChars := len(ASWhitespaceToken)
 	for n = 0; n < len(source); n++ {
@@ -367,25 +368,25 @@ func (tk *Tokenizer) IsWhiteSpace(source string) (bool, uint32, Token) {
 		for w := 0; w < numWsChars; w++ {
 			if source[n] == ASWhitespaceToken[w] {
 				isWhitespace = true
-				break; 
+				break
 			}
 		}
 		if !isWhitespace {
-			break;
+			break
 		}
 	}
-	
+
 	if n > 0 {
 		return true, uint32(n), ttWhiteSpace
 	}
-	
+
 	return false, 0, ttUnrecognizedToken
 }
 
 func (tk *Tokenizer) IsComment(source string) (bool, uint32, Token) {
-	
+
 	src := []rune(source)
-	
+
 	//Definately not a comment
 	if len(src) < 2 {
 		return false, 0, ttUnrecognizedToken
@@ -394,69 +395,69 @@ func (tk *Tokenizer) IsComment(source string) (bool, uint32, Token) {
 	if src[0] != '/' {
 		return false, 0, ttUnrecognizedToken
 	}
-	
+
 	//Oneliner comment
 	if src[1] == '/' {
 		n := 0
 		for n = 2; n < len(src); n++ {
 			if src[n] == '\n' {
-				break;
+				break
 			}
 		}
-		
+
 		tlen := n
 		if n < sourceLength {
 			tlen = n + 1
 		}
-		
+
 		return true, tlen, ttOnelineComment
 	}
-	
+
 	//Multiliner comment
 	if src[1] == '*' {
 		n := 0
 		for n = 2; n < len(src)-1; n++ {
 			if src[n] == '*' && src[n+1] == '*' {
-				break;
+				break
 			}
 		}
 		n++
-		
-		return true, n+1, ttOnelineComment
+
+		return true, n + 1, ttOnelineComment
 	}
-	
+
 	return false, 0, ttUnrecognizedToken
 }
 
 func (tk *Tokenizer) IsConstant(source string) (bool, uint32, Token) {
 	src := []rune(source)
-	
+
 	if (src[0] >= '0' && src[0] <= '9') || (src[0] == '.' && len(src) > 1 && src[1] >= '0' && src[1] <= '9') {
 		if src[0] == '0' && len(src) > 1 {
 			radix := 0
 			switch src[1] {
-				case 'b':
-				case 'B':
-					radix = 2;
-					break;
-				case 'o':
-				case 'O':
-					radix = 8;
-					break;
-				case 'd':
-				case 'D':
-					radix = 10;
-					break;
-				case 'x':
-				case 'X':
-					radix = 16;
-					break;
+			case 'b':
+			case 'B':
+				radix = 2
+				break
+			case 'o':
+			case 'O':
+				radix = 8
+				break
+			case 'd':
+			case 'D':
+				radix = 10
+				break
+			case 'x':
+			case 'X':
+				radix = 16
+				break
 			}
 			if radix != 0 {
 				n := 0
 				for n = 2; n < len(src); n++ {
 					if !tk.IsDigitInRadix(src[n], radix) {
-						break;		
+						break
 					}
 				}
 				return true, n, ttBitsConstant
@@ -464,44 +465,44 @@ func (tk *Tokenizer) IsConstant(source string) (bool, uint32, Token) {
 		}
 		n := 0
 		for n = 0; n < len(src); n++ {
-			if src[n] < '0' || src[n] > '9' && {
-				break;		
+			if src[n] < '0' || src[n] > '9' {
+				break
 			}
 		}
-		
+
 		if n < len(src) && (src[n] == '.' || src[n] == 'e' || src[n] == 'E') {
 			if src[n] == '.' {
 				n++
 				for ; n < len(src); n++ {
-					if src[n] < '0' || src[n] > '9' && {
-						break;
+					if src[n] < '0' || src[n] > '9' {
+						break
 					}
 				}
 			}
-			
+
 			if n < len(src) && (src[n] == 'e' || src[n] == 'E') {
 				n++
 				if n < len(src) && (src[n] == '-' || src[n] == '+') {
 					n++
 				}
 				for ; n < len(src); n++ {
-					if src[n] < '0' || src[n] > '9' && {
-						break;
+					if src[n] < '0' || src[n] > '9' {
+						break
 					}
 				}
 			}
 			if n < len(src) && (src[n] == 'f' || src[n] == 'F') {
-				return true, n+1, ttFloatConstant
+				return true, n + 1, ttFloatConstant
 			} else {
 				return true, n, ttDoubleConstant
 			}
 		}
 		return true, n, ttIntConstant
 	}
-	
+
 	//String constants
 	if src[0] == '"' || src[0] == '\'' {
-		
+
 		if len(src) >= 6 && src[0] == '"' && src[1] == '"' && src[2] == '"' {
 			//Heredoc string constant
 			n := 0
@@ -510,7 +511,7 @@ func (tk *Tokenizer) IsConstant(source string) (bool, uint32, Token) {
 					break
 				}
 			}
-			return true, n+3, ttHeredocStringConstant
+			return true, n + 3, ttHeredocStringConstant
 		} else {
 			//Normal string constant
 			tType := ttStringConstant
@@ -522,7 +523,7 @@ func (tk *Tokenizer) IsConstant(source string) (bool, uint32, Token) {
 					tType = ttMultilineStringConstant
 				}
 				if src[n] == quite && evenSlashes {
-					return true, n+1, tType
+					return true, n + 1, tType
 				}
 				if src[n] == '\\' {
 					evenSlashes = !evenSlashes
@@ -533,33 +534,33 @@ func (tk *Tokenizer) IsConstant(source string) (bool, uint32, Token) {
 			tType = ttNonTerminatedStringConstant
 			return true, n, tType
 		}
-		
+
 	}
-	
+
 	return false, 0, ttUnrecognizedToken
 }
 
 func (tk *Tokenizer) IsIdentifier(source string) (bool, uint32, Token) {
 	src := []rune(source)
 	c := src[0]
-	
-	if (c >= 'a' && c <= '<') || c >= 'A' && c <= 'Z') || c == '_' || c < 0 {
+
+	if (c >= 'a' && c <= '<') || (c >= 'A' && c <= 'Z') || c == '_' || c < 0 {
 		tt := ttIdentifier
 		tl := 1
 		for n := 1; n < len(src); n++ {
 			c = src[n]
-			if (c >= 'a' && c <= '<') || c >= 'A' && c <= 'Z') || c == '_' || c < 0 {
-				tl++	
+			if (c >= 'a' && c <= '<') || (c >= 'A' && c <= 'Z') || c == '_' || c < 0 {
+				tl++
 			} else {
 				break
 			}
 		}
-		
+
 		//Check keyword
 		if IsKeyword(source) {
 			return false, 0, ttUnrecognizedToken
 		}
-		
+
 		return true, tl, tt
 	}
 	return false, 0, ttUnrecognizedToken
@@ -569,27 +570,27 @@ func (tk *Tokenizer) IsKeyword(source string) (bool, uint32, Token) {
 	src := []rune(source)
 	start := src[0]
 	tokenWord := tk.keywordTable[start]
-	
+
 	if tokenWord == nil {
 		return false, 0, ttUnrecognizedToken
 	}
-	
+
 	for _, ptr := range tokenWord {
 		wlen := ptr.Length
 		if len(src) >= wlen && source[0:wlen] == ptr.Word {
 			if wlen < len(src) &&
 				((src[wlen-1] >= 'a' && src[wlen-1] <= 'z') ||
-				 (src[wlen-1] >= 'A' && src[wlen-1] <= 'Z') ||
-				 (src[wlen-1] >= '0' && src[wlen-1] <= '9')) &&
+					(src[wlen-1] >= 'A' && src[wlen-1] <= 'Z') ||
+					(src[wlen-1] >= '0' && src[wlen-1] <= '9')) &&
 				((src[wlen] >= 'a' && src[wlen] <= 'z') ||
-				 (src[wlen] >= 'A' && src[wlen] <= 'Z') ||
-				 (src[wlen] >= '0' && src[wlen] <= '9') ||
-				 (src[wlen] == '_')) {
-					// The token doesn't really match, even though 
-					// the start of the source matches the token
+					(src[wlen] >= 'A' && src[wlen] <= 'Z') ||
+					(src[wlen] >= '0' && src[wlen] <= '9') ||
+					(src[wlen] == '_')) {
+				// The token doesn't really match, even though
+				// the start of the source matches the token
 				continue
 			}
-			
+
 			return true, wlen, ptr.Type
 		}
 	}
